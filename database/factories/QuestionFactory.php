7a076efd589f567cc8989199b83c785c07cf3e75
tony <tony860725@gmail.com>
2019-06-24 16:56:43 +0800

    Factory and seed

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -1,9 +1,20 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
+use App\Category;
 
 $factory->define(App\Question::class, function (Faker $faker) {
+    $tittle = $faker->sentence;
     return [
-        //
+        'tittle' => $tittle,
+        'slug' => str_slug($tittle),
+        'body' => $faker->text,
+        'category_id' => function(){
+            return Category::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random();
+        },
     ];
 });
